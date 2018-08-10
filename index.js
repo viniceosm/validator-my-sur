1077b3a5ffe11f803759c36d6a62fda2d834ca55
viniceosm <miiller.vini@gmail.com>
2018-08-10 11:29:41 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var demo = new Vue({
+	el: '#app',
+	data: {
+		text: '',
+		resultado: ''
+	},
+	methods: {
+		validate() {
+			if (this.text.trim() !== '') {
+				let mySurname = (' M I I ' + ' L L ' + ' E R ').replace(/ /g, '') +
+				' ' + (' REBE LL O ').replace(/ /g, '');
+
+				if (this.text.split(' ').length !== 2) {
+					this.resultado = 'My surname is two';
+					return;
+				}
+
+				this.resultado = this.text.toLowerCase() === mySurname.toLowerCase() ? 'You are right' : 'You are wrong';
+			} else {
+				this.resultado = 'Type something.';
+			}
+		},
+		focusInput() {
+			this.$refs.surname.focus();
+		}
+	}
+});
