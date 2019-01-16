65ac34353960b5cfc3854fb148e0b0ebb978e09d
viniceosm <miiller.vini@gmail.com>
2019-01-16 08:57:34 -0200

    Accepts first name too

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # validator-my-sur
+
 App for validate my surname :blush:
 
 Util app for who will use my surname
--- index.js
+++ index.js
@@ -10,12 +10,15 @@ var demo = new Vue({
 				let mySurname = (' M I I ' + ' L L ' + ' E R ').replace(/ /g, '') +
 				' ' + (' REBE LL O ').replace(/ /g, '');
 
-				if (this.text.split(' ').length !== 2) {
-					this.resultado = 'My surname is two';
+				if (this.text.toLowerCase() === 'vinícius ' + mySurname.toLowerCase()) {
+					this.resultado = 'You are right, you typed my first name together.';
+					return;
+				} else if (this.text.split(' ').length !== 2) {
+					this.resultado = 'My surname is two.';
 					return;
 				}
 
-				this.resultado = this.text.toLowerCase() === mySurname.toLowerCase() ? 'You are right' : 'You are wrong';
+				this.resultado = this.text.toLowerCase() === mySurname.toLowerCase() ? 'You are right.' : 'You are wrong.';
 			} else {
 				this.resultado = 'Type something.';
 			}
